4dbdb202b1422a54d7426388955fc6169446ec1b
Pol Curto <polcurto@gmail.com>
2024-04-13 15:37:42 +0200

    New zone and new items. Basic functionalities implemented

--- World.cpp
+++ World.cpp
@@ -5,6 +5,7 @@
 
 World::World()
 {
+	// Rooms and exits
 	Room* startingRoom = new Room("Entrance", "A monumental iron door. Gives you the chills");
 	Room* forest = new Room("Dark forest", "You have never seen such an amount of trees together");
 	entities.push_back(startingRoom);
@@ -18,21 +19,47 @@ World::World()
 
 	Room* dungeon = new Room("Dungeon Main Hall", "Darkness and algo everywhere the eye can reach");
 	Exit* startToDungeon = new Exit("Tight Corridors", "Better not be claustrophobic", startingRoom, dungeon, RIGHT);
-	Exit* dungeonToStart = new Exit("Wide Corridors", "Enough space to not have all the ways covered", dungeon, startingRoom, LEFT);
+	Exit* dungeonToStart = new Exit("Wide Corridors", "Enough space for a deadly ambush", dungeon, startingRoom, LEFT);
 	startingRoom->AddChild(startToDungeon);
 	dungeon->AddChild(dungeonToStart);
 	entities.push_back(dungeon);
 	entities.push_back(startToDungeon);
 	entities.push_back(dungeonToStart);
 
-	Item* sword = new Item("Greatsword", "Better be strong to handle this blade", dungeon);
-	dungeon->AddChild(sword);
+	Room* chamber = new Room("King's Chamber", "The kingdom's mightiest treasures gathered in a single room");
+	Exit* dungeonToChamber = new Exit("Royal corridors", "You can smell the wealth of the place", dungeon, chamber, UP);
+	Exit* chamberToDungeon = new Exit("Royal corridors", "You can smell the wealth of the place", chamber, dungeon, DOWN);
+	Exit* forestToChamber = new Exit("Steep hill", "Drop yourself and pray to fall in a safe spot", forest, chamber, RIGHT);
+	Exit* chamberToForest = new Exit("Secret passage", "Can't build a castle without it's secret exit", dungeon, forest, LEFT);
+	dungeon->AddChild(dungeonToChamber);
+	forest->AddChild(forestToChamber);
+	chamber->AddChild(chamberToDungeon);
+	chamber->AddChild(chamberToForest);
+	entities.push_back(chamber);
+	entities.push_back(dungeonToChamber);
+	entities.push_back(chamberToDungeon);
+	entities.push_back(forestToChamber);
+	entities.push_back(chamberToForest);
+
+	// Items
+	Item* sword = new Item("Greatsword", "Better be strong to handle this blade", startingRoom);
+	startingRoom->AddChild(sword);
 	entities.push_back(sword);
 
 	Item* pearl = new Item("Zenimyte", "Its magic powers everything in contact with its surface", forest);
 	forest->AddChild(pearl);
 	entities.push_back(pearl);
 
+	Item* shield = new Item("Shield", "Can be used to take some hits, but won't last for long", dungeon);
+	dungeon->AddChild(shield);
+	entities.push_back(sword);
+
+	Item* potion = new Item("Potion", "The king's last resource. Gives you a boost you have nefer experienced", chamber);
+	chamber->AddChild(potion);
+	entities.push_back(potion);
+
+
+	// Player
 	player = new Player("Godfrey", "The first Elden Lord", startingRoom);
 	startingRoom->AddChild(player);
 	entities.push_back(player);
